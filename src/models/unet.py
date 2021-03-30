ff52c19a4500316303aba365b0d7f6f913cfcd70
Emeka Kanikwu <kanikwuemeka@gmail.com>
2021-03-30 01:48:26 -0500

    fix error

--- src/models/unet.py
+++ src/models/unet.py
@@ -7,165 +7,39 @@ import torch.nn.functional as F
 from src.models.layers import ConvBnRelu, UBlock, conv1x1, UBlockCbam, CBAM, InputTransition, DownTransition, UpTransition, OutputTransition
 
 
-# class VNet(nn.Module):
-#     # the number of convolutions in each layer corresponds
-#     # to what is in the actual prototxt, not the intent
-#     def __init__(self, inplanes, num_classes, width, norm_layer=None, deep_supervision=False, dropout=0,
-#                  **kwargs):
-#         super(VNet, self).__init__()
-
-#         features = [width * 2 ** i for i in range(4)]
-#         print(features)
-
-#         self.input_layer = InputTransition(inplanes, features[0], norm_layer)
-#         self.down_layer1 = DownTransition(features[0], 1, norm_layer)
-#         self.down_layer2 = DownTransition(features[1], 2, norm_layer)
-#         self.down_layer3 = DownTransition(features[2], 3, norm_layer, dropout=True)
-#         self.down_layer4 = DownTransition(features[3], 2, norm_layer, dropout=True)
-#         self.up_layer1 = UpTransition(features[3], features[3], 2, norm_layer, dropout=True)
-#         self.up_layer2 = UpTransition(features[3], features[2], 2, norm_layer, dropout=True)
-#         self.up_layer3 = UpTransition(features[2], features[1], 1, norm_layer)
-#         self.up_layer4 = UpTransition(features[1], features[0], 1, norm_layer)
-#         self.out_tr = OutputTransition(features[0], num_classes, norm_layer)
-
-#     def forward(self, x):
-#         in_lr = self.input_layer(x)
-#         down1 = self.down_layer1(in_lr)
-#         down2 = self.down_layer2(down1)
-#         down3 = self.down_layer3(down2)
-#         down4 = self.down_layer4(down3)
-#         out = self.up_layer1(down4, down3)
-#         out = self.up_layer2(out, down2)
-#         out = self.up_layer3(out, down1)
-#         out = self.up_layer4(out, in_lr)
-#         out = self.out_tr(out)
-#         return out
-
-
-class conv3d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, activation_func=nn.PReLU):
-        """
-        + Instantiate modules: conv-relu-norm
-        + Assign them as member variables
-        """
-        super(conv3d, self).__init__()
-        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size, padding=1)
-        self.relu = activation_func()
-        # with learnable parameters
-        # self.norm = nn.InstanceNorm3d(out_channels, affine=True)
-
-    def forward(self, x):
-        return self.relu(self.conv(x))
-
-
-class conv3d_x3(nn.Module):
-    """Three serial convs with a residual connection.
-    Structure:
-        inputs --> ① --> ② --> ③ --> outputs
-                   ↓ --> add--> ↑
-    """
-
-    def __init__(self, in_channels, out_channels, kernel_size=3):
-        super(conv3d_x3, self).__init__()
-        self.conv_1 = conv3d(in_channels, out_channels, kernel_size)
-        self.conv_2 = conv3d(out_channels, out_channels, kernel_size)
-        self.conv_3 = conv3d(out_channels, out_channels, kernel_size)
-
-    def forward(self, x):
-        z_1 = self.conv_1(x)
-        z_3 = self.conv_3(self.conv_2(z_1))
-        return z_1 + z_3
-
-
-class deconv3d_x3(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=2, activation_func=nn.PReLU):
-        super(deconv3d_x3, self).__init__()
-        self.up = deconv3d_as_up(in_channels, out_channels, kernel_size, stride)
-        self.lhs_conv = conv3d(out_channels // 2, out_channels, kernel_size)
-        self.conv_x3 = conv3d_x3(out_channels, out_channels, kernel_size)
-
-    def forward(self, lhs, rhs):
-        rhs_up = self.up(rhs)
-        lhs_conv = self.lhs_conv(lhs)
-        rhs_add = crop(rhs_up, lhs_conv) + lhs_conv
-        return self.conv_x3(rhs_add)
-
-
-def crop(large, small):
-    """large / small with shape [batch_size, channels, depth, height, width]"""
-
-    l, s = large.size(), small.size()
-    offset = [0, 0, (l[2] - s[2]) // 2, (l[3] - s[3]) // 2, (l[4] - s[4]) // 2]
-    return large[..., offset[2]: offset[2] + s[2], offset[3]: offset[3] + s[3], offset[4]: offset[4] + s[4]]
-
-
-def conv3d_as_pool(in_channels, out_channels, kernel_size=3, stride=2, activation_func=nn.ReLU):
-    return nn.Sequential(
-        nn.Conv3d(in_channels, out_channels, kernel_size, stride, padding=1),
-        activation_func())
-
-
-def deconv3d_as_up(in_channels, out_channels, kernel_size=3, stride=2, activation_func=nn.ReLU):
-    return nn.Sequential(
-        nn.ConvTranspose3d(in_channels, out_channels, kernel_size, stride),
-        activation_func()
-    )
-
-
-class softmax_out(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(softmax_out, self).__init__()
-        self.conv_1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.conv_2 = nn.Conv3d(out_channels, out_channels, kernel_size=1, padding=0)
-        self.softmax = F.softmax
-
-    def forward(self, x):
-        """Output with shape [batch_size, 1, depth, height, width]."""
-        # Do NOT add normalize layer, or its values vanish.
-        y_conv = self.conv_2(self.conv_1(x))
-        # Put channel axis in the last dim for softmax.
-        y_perm = y_conv.permute(0, 2, 3, 4, 1).contiguous()
-        y_flat = y_perm.view(-1, 2)
-        return self.softmax(y_flat)
-
-
 class VNet(nn.Module):
-    def __init__(self, inplanes, num_classes, width, norm_layer=None, deep_supervision=False, dropout=0, **kwargs):
-        features = [width * 2 ** i for i in range(4)]
-
+    # the number of convolutions in each layer corresponds
+    # to what is in the actual prototxt, not the intent
+    def __init__(self, inplanes, num_classes, width, norm_layer=None, deep_supervision=False, dropout=0,
+                 **kwargs):
         super(VNet, self).__init__()
-        self.conv_1 = conv3d_x3(inplanes, features[0])
-        self.pool_1 = conv3d_as_pool(features[0], features[1])
-        self.conv_2 = conv3d_x3(features[1], features[1])
-        self.pool_2 = conv3d_as_pool(features[1], features[2])
-        self.conv_3 = conv3d_x3(features[2], features[2])
-        self.pool_3 = conv3d_as_pool(features[2], features[3])
-        self.conv_4 = conv3d_x3(features[3], features[3])
-        self.pool_4 = conv3d_as_pool(features[3], features[3] * 2)
 
-        self.bottom = conv3d_x3(features[3]*2, features[3]*2)
+        features = [width * 2 ** i for i in range(4)]
+        print(features)
 
-        self.deconv_4 = deconv3d_x3(features[3] * 2, features[3] * 2)
-        self.deconv_3 = deconv3d_x3(features[3], features[2])
-        self.deconv_2 = deconv3d_x3(features[2], features[1])
-        self.deconv_1 = deconv3d_x3(features[1], features[0])
-
-        self.out = softmax_out(features[0], num_classes)
+        self.input_layer = InputTransition(inplanes, features[0], norm_layer)
+        self.down_layer1 = DownTransition(features[0], 1, norm_layer)
+        self.down_layer2 = DownTransition(features[1], 2, norm_layer)
+        self.down_layer3 = DownTransition(features[2], 3, norm_layer, dropout=True)
+        self.down_layer4 = DownTransition(features[3], 2, norm_layer, dropout=True)
+        self.up_layer1 = UpTransition(features[3], features[3], 2, norm_layer, dropout=True)
+        self.up_layer2 = UpTransition(features[3], features[2], 2, norm_layer, dropout=True)
+        self.up_layer3 = UpTransition(features[2], features[1], 1, norm_layer)
+        self.up_layer4 = UpTransition(features[1], features[0], 1, norm_layer)
+        self.out_tr = OutputTransition(features[0], num_classes, norm_layer)
 
     def forward(self, x):
-        conv_1 = self.conv_1(x)
-        pool = self.pool_1(conv_1)
-        conv_2 = self.conv_2(pool)
-        pool = self.pool_2(conv_2)
-        conv_3 = self.conv_3(pool)
-        pool = self.pool_3(conv_3)
-        bottom = self.bottom(pool)
-        deconv = self.deconv_4(conv_4, bottom)
-        deconv = self.deconv_3(conv_3, deconv)
-        deconv = self.deconv_2(conv_2, deconv)
-        deconv = self.deconv_1(conv_1, deconv)
-        return self.out(deconv)
-
+        in_lr = self.input_layer(x)
+        down1 = self.down_layer1(in_lr)
+        down2 = self.down_layer2(down1)
+        down3 = self.down_layer3(down2)
+        down4 = self.down_layer4(down3)
+        out = self.up_layer1(down4, down3)
+        out = self.up_layer2(out, down2)
+        out = self.up_layer3(out, down1)
+        out = self.up_layer4(out, in_lr)
+        out = self.out_tr(out)
+        return out
 
 
 class Unet(nn.Module):
